820a187d21876e17373f179fb37bcbb3f0c85e6c
YF Chau <yaufai.chau@gmail.com>
2025-04-25 11:03:04 +0100

    Refactor locator to a seperate file

---
+++ locators.py
@@ -0,0 +1,24 @@
+# locators.py
+
+# Login Page
+LOGIN_EMAIL_INPUT_SELECTOR = "input[type='email'][name='email']"
+LOGIN_CONTINUE_BUTTON_SELECTOR = "role=button[name='Continue']"
+LOGIN_PASSWORD_INPUT_SELECTOR = "input[type='password'][name='password']"
+LOGIN_SIGN_IN_BUTTON_SELECTOR = "role=button[name='Sign in']"
+LOGIN_ERROR_MESSAGE_SELECTOR = "div.error-message"
+
+# Login Option
+PASSWORD_LOGIN_LINK_SELECTOR = (
+    "a.login-option.substack-login__login-option:has-text('Sign in with password')"
+)
+
+# Post-Login / Dashboard
+DASHBOARD_INDICATOR_SELECTOR = (
+    "button[data-href*='/publish/home']:has-text('Dashboard')"
+)
+NEW_POST_BUTTON_SELECTOR = "button:has-text('New post')"
+NEW_TEXT_POST_BUTTON_SELECTOR = "role=link[name='Text post']"
+
+# Editor Page
+POST_TITLE_INPUT_SELECTOR = "textarea[placeholder='Title']"
+POST_CONTENT_EDITOR_SELECTOR = "div.tiptap.ProseMirror"
